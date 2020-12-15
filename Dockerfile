4b5287305b548a6478e0233fbe3ae837bffc4288
Tamara Lazarevic <lazarevict97@gmail.com>
2020-12-15 19:00:30 +0100

    Add files via upload

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM maven:3.6.3-jdk-11 AS maven
+FROM maven:3.6.3-jdk-11-slim AS maven
 
 WORKDIR maven
 
@@ -7,7 +7,7 @@ COPY demo/demo .
 RUN mvn package -DskipTests
 
 
-FROM openjdk:8
+FROM adoptopenjdk/openjdk11:jdk-11.0.9_11.1-alpine-slim
 
 EXPOSE 8081
 
--- demo/demo/src/main/java/com/example/demo/TestController.java
+++ demo/demo/src/main/java/com/example/demo/TestController.java
@@ -1,20 +1,36 @@
 package com.example.demo;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.repo.CounterRepo;
+import com.example.demo.data.Counter;
+import java.util.List;
 
 //http://localhost:8080/test/hm --> putanja, samo stigne json za sad
 @RestController
 @RequestMapping(value = "/test")
 public class TestController {
+	
+	@Autowired
+	CounterRepo repo;
  
-	 @GetMapping(value = "/hm", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<String> getTestData() {
-        return new ResponseEntity<String>("Working", HttpStatus.OK);
+	 @GetMapping(value = "/hm")
+    public String getTestData() {
+		List<Counter> counters = (List<Counter>) repo.findAll();
+		Counter c = counters.get(0);
+		Long number = c.getCounter();
+		number=number+1;
+		c.setCounter(number);
+		repo.save(c);
+		
+        return new String(number.toString());
     }
 }
